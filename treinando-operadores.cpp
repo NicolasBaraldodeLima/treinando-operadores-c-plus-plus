b68d9d2ccf29d13d4ab6615204088b592f7c42b6
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-09-20 14:12:39 -0300

    Add files via upload

---
+++ treinando-operadores.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int n1, n2;
+
+int main() {
+	
+	int n3,n4;
+	int res;
+	
+	n1=11;
+	n2=3;
+	n3=5;
+	n4=2;
+	
+	res = n1 + n2 + n3 + n4;
+	
+	cout << "Soma de todas as variáveis: " << res << "\n\n";
+	
+	return 0;
+	
+}
